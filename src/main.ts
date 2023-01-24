86a8e31cae90be4626f3369074a747ce27756877
0xBriz <chaindevone@gmail.com>
2023-01-24 07:45:25 -0500

    testing ve stake for user using this lib

--- src/main.ts
+++ src/main.ts
@@ -29,19 +29,29 @@ import {
   doGaugeDeposit,
 } from './services/gauges/gauge-utils';
 import { formatEther, parseEther } from 'ethers/lib/utils';
+import { stakeForUser } from './services/gauges/voting-escrow';
 
 async function run() {
-  console.log('vertekfi run:');
+  console.log('VertekFi run:');
   config({ path: join(process.cwd(), '.env') });
   // console.log(calcOutGivenIn(17000, 0.8, 30000, 0.2, 1));
 
+  // await testVeStakeFor();
+}
+
+async function testVeStakeFor() {
+  const niceTestAddy = '0x1555D126e096A296A5870A566db224FD9Cf72f03';
+  await stakeForUser(niceTestAddy, parseEther('10'), 30);
+}
+
+async function testGaugeRewardDeposits() {
   const vrtkAddy = '0xa5694789C0BaED77d16ca36edC45C9366DBFe0A9';
   const ashareGauge = '0x53c5B5C391FD8d7f538fb6Ac6E50Ec47e0680CE0';
   const amesGauge = '0xa64DE16c3D674F4F56aa5b8978eCeb4C2Cceb7A9';
   const vrtkBusdGauge = '0xdcae01e5f3103178Cf06EB3037c9b8E5FA9FD848';
 
-  // await addRewardTokenToGauge(ashareGauge, vrtkAddy);
-  // await deGaugeRewardTokenDeposit(ashareGauge, vrtkAddy, parseEther('100'));
+  await addRewardTokenToGauge(ashareGauge, vrtkAddy);
+  await deGaugeRewardTokenDeposit(ashareGauge, vrtkAddy, parseEther('100'));
 }
 
 run();
--- src/services/gauges/voting-escrow.ts
+++ src/services/gauges/voting-escrow.ts
@@ -1,10 +1,28 @@
-import { parseEther } from 'ethers/lib/utils';
-import moment from 'moment';
+import { BigNumber, ContractReceipt } from 'ethers';
+import { formatEther, parseEther } from 'ethers/lib/utils';
+import * as moment from 'moment';
 import { getSignerAddress } from 'src/utils/account.util';
 import { getVotingEscrow } from 'src/utils/contract.utils';
 import { approveTokensIfNeeded } from 'src/utils/token.utils';
 import { awaitTransactionComplete } from 'src/utils/transaction.utils';
 import { getMainPoolConfig } from '../pools/pool.utils';
+
+import * as fs from 'fs-extra';
+import { join } from 'path';
+
+export enum VotingEscrowStakeType {
+  CREATE = 'CREATE',
+  INCREASE_AMOUNT = 'INCREASE_AMOUNT',
+  INCREASE_TOTAL_STAKE = 'INCREASE_TOTAL_STAKE',
+}
+
+export interface EscrowStakeForInfo {
+  txHash: string;
+  amount: string;
+  who: string;
+  endTimestamp: number;
+  type: VotingEscrowStakeType;
+}
 
 /**
  *  FeeDistributor needs ve total supply > 0 if start time is the current week/epoch
@@ -25,8 +43,46 @@ export async function doInitialVotingEscrowDeposit() {
   );
 }
 
-export async function stakeForUser(who: string) {
-  //
+/**
+ * Permissioned call. Authorization should already have been given.
+ * The account calling this will need to set as the "staking admin" in the contract.
+ * As well as have the `amount` in their account at call time.
+ * @param who
+ * @param amount
+ * @param endTimeDaysFromNow  The number of days from now the lock should end.
+ */
+export async function stakeForUser(
+  who: string,
+  amount: BigNumber,
+  endTimeDaysFromNow: number,
+) {
+  const mainPool = await getMainPoolConfig();
+  const votingEsrow = await getVotingEscrow();
+  // Could add certain checks here and account for contracts rounding of time to weeks, etc
+  // But the way this is used right now is more of a manual interactive process anyway
+  const unlockTime = moment()
+    .add(endTimeDaysFromNow, 'days')
+    .endOf('day')
+    .utc()
+    .unix();
+
+  await approveTokensIfNeeded(
+    [mainPool.poolAddress],
+    await getSignerAddress(),
+    votingEsrow.address,
+  );
+
+  const receipt = await awaitTransactionComplete(
+    votingEsrow.admin_create_lock_for(who, amount, unlockTime),
+  );
+
+  await saveStake(
+    who,
+    amount,
+    unlockTime,
+    VotingEscrowStakeType.CREATE,
+    receipt,
+  );
 }
 
 export async function increaseStakeForUser() {
@@ -35,4 +91,28 @@ export async function increaseStakeForUser() {
 
 export async function increaseTotalStakeForUser() {
   //
+}
+
+async function saveStake(
+  who: string,
+  amount: BigNumber,
+  endTimestamp: number,
+  type: VotingEscrowStakeType,
+  receipt: ContractReceipt,
+) {
+  const dataPath = join(
+    process.cwd(),
+    'src/data/vertek/ve-stakes/ve-stakes.json',
+  );
+
+  const stakes: EscrowStakeForInfo[] = await fs.readJSON(dataPath);
+  stakes.push({
+    who,
+    amount: formatEther(amount),
+    endTimestamp,
+    type,
+    txHash: receipt.transactionHash,
+  });
+
+  await fs.writeJson(dataPath, stakes);
 }
